a416d0783dc7c377c9d82879e9b9e2da19a22d01
azzu <Azhan@willfly.com>
2023-08-11 11:17:56 +0530

    deleted copy paste

--- js.js
+++ js.js
@@ -41,21 +41,3 @@ let ShowHack = async (message) =>{
 
 
 
-
-// const sleep = async (seconds) => {
-//     return new Promise((resolve, reject) => {
-//             setTimeout(() => { resolve(true) }, seconds * 1000)
-//     })
-// }
-
-// const showHack = async (message) => {
-//     await sleep(2)
-//     // console.log(message)
-//     text.innerHTML = text.innerHTML + message + "<br>"
-// }
-
-// (async () => {
-//     for (let i = 0; i < a.length; i++) {
-//             await showHack(a[i])
-//     }
-// })()
